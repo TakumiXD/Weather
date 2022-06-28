04d7581c99b6194778990db2e2db705ff1bc7045
tsugita26 <85015271+tsugita26@users.noreply.github.com>
2022-06-27 21:04:55 -0700

    updated layout files

--- app/src/test/java/com/example/weatherapp/MainActivityTest.java
+++ app/src/test/java/com/example/weatherapp/MainActivityTest.java
@@ -9,6 +9,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 import android.content.Context;
 import android.content.Intent;
