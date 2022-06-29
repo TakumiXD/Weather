d6630357b707d4c0f58bc9b5e4eb2e547b7d448a
tsugita26 <85015271+tsugita26@users.noreply.github.com>
2022-06-29 16:19:46 -0700

    Added test for long city names

--- app/src/test/java/com/example/weatherapp/searchBehaviorTest.java
+++ app/src/test/java/com/example/weatherapp/searchBehaviorTest.java
@@ -1,6 +1,7 @@
 package com.example.weatherapp;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 import android.content.Context;
 import android.content.Intent;
@@ -15,8 +16,12 @@ import org.junit.runner.RunWith;
 
 @RunWith(AndroidJUnit4.class)
 public class searchBehaviorTest {
+    private static final String BASIC_CITY = "London";
+    private static final String LONG_CITY = "Bonadelle Ranchos-Madera Ranchos";
+    private static final String NEW_LINE = "\n";
+
     @Test
-    public void testSearch() {
+    public void basicSearch() {
         Context context = ApplicationProvider.getApplicationContext();
         Intent intent = new Intent(context, MainActivity.class);
         intent.putExtra(MainActivity.ENABLE_GPS_INTENT, false);
@@ -25,11 +30,31 @@ public class searchBehaviorTest {
 
         scenario.onActivity(activity -> {
             activity.etSearchBar.requestFocus();
-            activity.etSearchBar.setText("London");
+            activity.etSearchBar.setText(BASIC_CITY);
             activity.etSearchBar.clearFocus();
-            assertEquals("London", activity.getEtSearchBar().getText().toString());
+            assertEquals(BASIC_CITY, activity.getEtSearchBar().getText().toString());
             activity.ibSearchButton.performClick();
-            assertEquals("London", activity.getPresenter().getCurrentSearchedCityName());
+            assertEquals(BASIC_CITY, activity.getPresenter().getCurrentSearchedCityName());
+        });
+    }
+
+    @Test
+    public void longNameSearch() {
+        Context context = ApplicationProvider.getApplicationContext();
+        Intent intent = new Intent(context, MainActivity.class);
+        intent.putExtra(MainActivity.ENABLE_GPS_INTENT, false);
+        ActivityScenario<MainActivity> scenario = ActivityScenario.<MainActivity>launch(intent);
+        scenario.moveToState(Lifecycle.State.CREATED);
+
+        scenario.onActivity(activity -> {
+            activity.etSearchBar.requestFocus();
+            activity.etSearchBar.setText(LONG_CITY);
+            activity.etSearchBar.clearFocus();
+            assertFalse(activity.getEtSearchBar().getText().toString().contains(NEW_LINE));
+            assertEquals(LONG_CITY, activity.getEtSearchBar().getText().toString());
+            activity.ibSearchButton.performClick();
+            assertFalse(activity.getPresenter().getCurrentSearchedCityName().contains(NEW_LINE));
+            assertEquals(LONG_CITY, activity.getPresenter().getCurrentSearchedCityName());
         });
     }
 }
