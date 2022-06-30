57b5bccf98a5cb0ce515adf0fb5645122d2a0ad8
tsugita26 <85015271+tsugita26@users.noreply.github.com>
2022-06-30 15:56:57 -0700

    Created 3 files
    FavoriteCitiesDatabase.java
    FavoriteCity.java
    FavoriteCityDao.java

---
+++ app/src/main/java/com/example/weatherapp/FavoriteCity.java
@@ -0,0 +1,19 @@
+package com.example.weatherapp;
+
+import androidx.annotation.NonNull;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "favorite_cities")
+public class FavoriteCity {
+
+    @PrimaryKey(autoGenerate = true)
+    public long id = 0;
+
+    @NonNull
+    public String name;
+
+    FavoriteCity(@NonNull String name) {
+        this.name = name;
+    }
+}
---
+++ app/src/main/java/com/example/weatherapp/FavoriteCityDao.java
@@ -0,0 +1,22 @@
+package com.example.weatherapp;
+
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
+public interface FavoriteCityDao {
+    @Insert
+    long insert(FavoriteCity favoriteCity);
+
+    @Query("SELECT * FROM `favorite_cities` WHERE `id`=:id")
+    FavoriteCity get (long id);
+
+    @Query("SELECT * FROM `favorite_cities`")
+    List<FavoriteCity> getAll();
+
+    @Query("DELETE FROM `favorite_cities` WHERE `name`=:name")
+    void delete(String name);
+}
